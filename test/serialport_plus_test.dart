ba10f68a6dd88a34c663affe04604719aede9523
sgsm74 <saeed.razii93@gmail.com>
2023-08-07 13:21:11 +0330

    chore: test

--- test/serialport_plus_test.dart
+++ test/serialport_plus_test.dart
@@ -1,29 +1,44 @@
+import 'dart:typed_data';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:serialport_plus/serialport_plus.dart';
-import 'package:serialport_plus/serialport_plus_platform_interface.dart';
-import 'package:serialport_plus/serialport_plus_method_channel.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
 class MockSerialportPlusPlatform
     with MockPlatformInterfaceMixin
     implements SerialportPlusPlatform {
+  void main() {
+    final SerialportPlusPlatform initialPlatform =
+        SerialportPlusPlatform.instance;
+
+    test('$MethodChannelSerialportPlus is the default instance', () {
+      expect(initialPlatform, isInstanceOf<MethodChannelSerialportPlus>());
+    });
+  }
 
   @override
-  Future<String?> getPlatformVersion() => Future.value('42');
-}
+  Future<bool?> close() {
+    throw UnimplementedError();
+  }
 
-void main() {
-  final SerialportPlusPlatform initialPlatform = SerialportPlusPlatform.instance;
+  @override
+  Future<List?> getAllDevices() {
+    throw UnimplementedError();
+  }
 
-  test('$MethodChannelSerialportPlus is the default instance', () {
-    expect(initialPlatform, isInstanceOf<MethodChannelSerialportPlus>());
-  });
+  @override
+  Future<List?> getAllDevicesPath() {
+    throw UnimplementedError();
+  }
 
-  test('getPlatformVersion', () async {
-    SerialportPlus serialportPlusPlugin = SerialportPlus();
-    MockSerialportPlusPlatform fakePlatform = MockSerialportPlusPlatform();
-    SerialportPlusPlatform.instance = fakePlatform;
+  @override
+  Future<bool?> open(
+      String filePath, int baudrate, int dataBits, int parity, int stopBits) {
+    throw UnimplementedError();
+  }
 
-    expect(await serialportPlusPlugin.getPlatformVersion(), '42');
-  });
+  @override
+  Future<bool?> write(Uint8List data) {
+    throw UnimplementedError();
+  }
 }
