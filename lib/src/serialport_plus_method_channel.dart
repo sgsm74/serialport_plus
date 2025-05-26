c9c04617614d08df3019d4b253dfc72ab7698f73
sgsm74 <s.gh4286@gmail.com>
2025-05-26 09:10:40 +0330

    Fix: variable spell

--- lib/src/serialport_plus_method_channel.dart
+++ lib/src/serialport_plus_method_channel.dart
@@ -8,7 +8,7 @@ class MethodChannelSerialportPlus extends SerialportPlusPlatform {
   /// The method channel used to interact with the native platform.
   @visibleForTesting
   final methodChannel = const MethodChannel('serialport_plus');
-  final eventChanel = const EventChannel('serialport_plus/event');
+  final eventChannel = const EventChannel('serialport_plus/event');
 
   @override
   Future<List?> getAllDevices() async {
@@ -18,36 +18,26 @@ class MethodChannelSerialportPlus extends SerialportPlusPlatform {
 
   @override
   Future<List?> getAllDevicesPath() async {
-    final devicesPath =
-        await methodChannel.invokeMethod<List>('getAllDevicesPath');
+    final devicesPath = await methodChannel.invokeMethod<List>('getAllDevicesPath');
     return devicesPath;
   }
 
   @override
-  Future<bool?> open(String filePath, int baudrate, int dataBits, int parity,
-      int stopBits) async {
-    final result = await methodChannel.invokeMethod<bool>('open', {
-      "devicePath": filePath,
-      "baudrate": baudrate,
-      "dataBits": dataBits,
-      "parity": parity,
-      "stopBits": stopBits
-    });
+  Future<bool?> open(String filePath, int baudrate, int dataBits, int parity, int stopBits) async {
+    final result = await methodChannel.invokeMethod<bool>(
+        'open', {"devicePath": filePath, "baudrate": baudrate, "dataBits": dataBits, "parity": parity, "stopBits": stopBits});
     return result;
   }
 
   @override
   Future<bool?> write(Uint8List data) async {
-    final result =
-        await methodChannel.invokeMethod<bool>('write', {"data": data});
+    final result = await methodChannel.invokeMethod<bool>('write', {"data": data});
     return result;
   }
 
   @override
   Stream<Uint8List> read() {
-    return eventChanel
-        .receiveBroadcastStream()
-        .map<Uint8List>((dynamic value) => value);
+    return eventChannel.receiveBroadcastStream().map<Uint8List>((dynamic value) => value);
   }
 
   @override
